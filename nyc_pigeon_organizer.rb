10b6b4506af8602f98a89600e704806f2662ad42
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-15 13:17:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,7 +18,7 @@ def nyc_pigeon_organizer(data)
  data.each_with_object( {} ) do |(k,v), a|
    v.each do |inner_key, names|
     names.each do |name|
-      if ! a[name]
+      if !a[name]
       a[name] = { }
   #     if ! a[name][key]
   #       a[name][key].Array
