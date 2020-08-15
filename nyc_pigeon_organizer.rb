c5790d92ddb8859582f127db27b67a938417e855
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-15 13:13:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
+require 'pry'
+pd = {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+ data.each_with_object( {} ) do |(k,v), a|
+   v.each do |inner_key, names|
+    # names.each do |name|
+    #   binding.pry
+    #   if ! a[name]
+    #     a[name] = { }
+    #   end
+    #   binding.pry
+    # end
+  #     if ! a[name][key]
+  #       a[name][key].Array
+  #     end
+   binding.pry
+  end
