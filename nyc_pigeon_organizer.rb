3e4b7d68bf007502d5ab62a515b6a48ade98e3ce
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-15 14:25:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,37 @@
+require 'pry'
+pd = {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+ data.each_with_object( {} ) do |(k,v), new_hash|
+   v.each do |inner_key, names|
+    names.each do |att|
+      if !new_hash[att]
+        new_hash[att] = { }
+      end
+      if ! new_hash[att][k]
+        new_hash[att][k] = [ ]
+      end
+      if ! new_hash[att][k].include?(inner_key)
+        new_hash[att][k] << inner_key.to_s
+        end
+      end
+      end
+    end
+    end
+
+
+
+
